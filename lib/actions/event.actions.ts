3843309d56c2fa1419cc9bd63ac8b3870bc660f5
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-12-27 08:35:07 +0100

    Worked on Events Details page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,13 @@ import { connectToDatabase } from "../Database"
 import User from "../Database/models/user.model"
 import Event from "../Database/models/event.model"
 import { handleError } from "../utils"
+import Category from "../Database/models/category.model"
+
+const populateEvent = (query: any) => {
+  return query
+    .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+    .populate({ path: 'category', model: Category, select: '_id name' })
+}
 
 
 
@@ -19,6 +26,20 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     // revalidatePath(path)
 
     return JSON.parse(JSON.stringify(newEvent))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+export async function getEventById(eventId: string) {
+  try {
+    await connectToDatabase()
+
+    const event = await populateEvent(Event.findById(eventId))
+
+    if (!event) throw new Error('Event not found')
+
+    return JSON.parse(JSON.stringify(event))
   } catch (error) {
     handleError(error)
   }
