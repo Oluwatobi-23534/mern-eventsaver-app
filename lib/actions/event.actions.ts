6a6209138ee96f0ac3407194879287a8ab24b812
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-12-28 12:42:57 +0100

    Started work on collection component on the homepage

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { CreateEventParams } from "@/types"
+import { CreateEventParams, GetAllEventsParams } from "@/types"
 import { connectToDatabase } from "../Database"
 import User from "../Database/models/user.model"
 import Event from "../Database/models/event.model"
@@ -40,6 +40,28 @@ export async function getEventById(eventId: string) {
     if (!event) throw new Error('Event not found')
 
     return JSON.parse(JSON.stringify(event))
+  } catch (error) {
+    handleError(error)
+  }
+}
+export async function getAllEvents({ query, limit = 6, page, category }: GetAllEventsParams) {
+  try {
+    await connectToDatabase()
+
+    const conditions = {};
+
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: 'desc' })
+      .skip(0)
+      .limit(limit);
+    
+    const events = await populateEvent(eventsQuery);
+    const eventsCount = await Event.countDocuments(conditions);
+
+    return {
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit),
+    }
   } catch (error) {
     handleError(error)
   }
