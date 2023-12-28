9af09d0fcfbf1daa61ea52f124433011c6e737bf
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-12-28 13:56:44 +0100

    Worked on Delete Confirmation Dialog

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -4,6 +4,8 @@ import { auth } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { DeleteConfirmation } from './DeleteConfirmation'
+
 
 type CardProps = {
     event: IEvent,
@@ -19,14 +21,16 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
 
     return (
         <div className='group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]'>
-            <Link href={`/events/${event._id}`} style={{ backgroundImage: `url(${event.imageUrl})` }} className='flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500'/>
+            <Link href={`/events/${event._id}`} style={{ backgroundImage: `url(${event.imageUrl})` }} className='flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500' />
             {/* is event creator */}
 
             {isEventCreator && !hidePrice && (
                 <div className='absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all'>
                     <Link href={`/events/${event._id}/update`}>
-                        <Image src="/assets/icons/edit.svg" alt='edit' width={20} height={20}/>
+                        <Image src="/assets/icons/edit.svg" alt='edit' width={20} height={20} />
                     </Link>
+
+                    <DeleteConfirmation eventId={event._id} />
                 </div>
             )}
 
@@ -46,7 +50,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
                 </p>
 
                 <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">{event.title}</p>
-                
+
                 <div className="flex-between w-full">
                     <p className="p-medium-14 md:p-medium-16 text-grey-600">
                         {event.organizer.firstName} {event.organizer.lastName}
@@ -55,12 +59,12 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
                     {hasOrderLink && (
                         <Link href={`/orders?eventId=${event._id}`} className='flex gap-2'>
                             <p className='text-primary-500'>Order Details</p>
-                            <Image src="/assets/icons/arrow.svg" alt="search" width={10} height={10}/>
+                            <Image src="/assets/icons/arrow.svg" alt="search" width={10} height={10} />
                         </Link>
                     )}
                 </div>
             </Link>
-            
+
         </div>
     )
 }
--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,11 +1,25 @@
 'use server'
 
-import { CreateEventParams, GetAllEventsParams } from "@/types"
-import { connectToDatabase } from "../Database"
-import User from "../Database/models/user.model"
-import Event from "../Database/models/event.model"
-import { handleError } from "../utils"
-import Category from "../Database/models/category.model"
+import { revalidatePath } from 'next/cache'
+
+import { connectToDatabase } from '@/lib/Database'
+import Event from '@/lib/Database/models/event.model'
+import User from '@/lib/Database/models/user.model'
+import Category from '@/lib/Database/models/category.model'
+import { handleError } from '@/lib/utils'
+
+import {
+  CreateEventParams,
+  UpdateEventParams,
+  DeleteEventParams,
+  GetAllEventsParams,
+  GetEventsByUserParams,
+  GetRelatedEventsByCategoryParams,
+} from '@/types'
+
+const getCategoryByName = async (name: string) => {
+  return Category.findOne({ name: { $regex: name, $options: 'i' } })
+}
 
 const populateEvent = (query: any) => {
   return query
@@ -13,8 +27,7 @@ const populateEvent = (query: any) => {
     .populate({ path: 'category', model: Category, select: '_id name' })
 }
 
-
-
+// CREATE
 export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
     await connectToDatabase()
@@ -23,7 +36,7 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     if (!organizer) throw new Error('Organizer not found')
 
     const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
-    // revalidatePath(path)
+    revalidatePath(path)
 
     return JSON.parse(JSON.stringify(newEvent))
   } catch (error) {
@@ -31,6 +44,7 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
   }
 }
 
+// GET ONE EVENT BY ID
 export async function getEventById(eventId: string) {
   try {
     await connectToDatabase()
@@ -44,24 +58,115 @@ export async function getEventById(eventId: string) {
     handleError(error)
   }
 }
+
+// UPDATE
+export async function updateEvent({ userId, event, path }: UpdateEventParams) {
+  try {
+    await connectToDatabase()
+
+    const eventToUpdate = await Event.findById(event._id)
+    if (!eventToUpdate || eventToUpdate.organizer.toHexString() !== userId) {
+      throw new Error('Unauthorized or event not found')
+    }
+
+    const updatedEvent = await Event.findByIdAndUpdate(
+      event._id,
+      { ...event, category: event.categoryId },
+      { new: true }
+    )
+    revalidatePath(path)
+
+    return JSON.parse(JSON.stringify(updatedEvent))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// DELETE
+export async function deleteEvent({ eventId, path }: DeleteEventParams) {
+  try {
+    await connectToDatabase()
+
+    const deletedEvent = await Event.findByIdAndDelete(eventId)
+    if (deletedEvent) revalidatePath(path)
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// GET ALL EVENTS
 export async function getAllEvents({ query, limit = 6, page, category }: GetAllEventsParams) {
   try {
     await connectToDatabase()
 
-    const conditions = {};
+    const titleCondition = query ? { title: { $regex: query, $options: 'i' } } : {}
+    const categoryCondition = category ? await getCategoryByName(category) : null
+    const conditions = {
+      $and: [titleCondition, categoryCondition ? { category: categoryCondition._id } : {}],
+    }
 
+    const skipAmount = (Number(page) - 1) * limit
     const eventsQuery = Event.find(conditions)
       .sort({ createdAt: 'desc' })
-      .skip(0)
-      .limit(limit);
-    
-    const events = await populateEvent(eventsQuery);
-    const eventsCount = await Event.countDocuments(conditions);
+      .skip(skipAmount)
+      .limit(limit)
+
+    const events = await populateEvent(eventsQuery)
+    const eventsCount = await Event.countDocuments(conditions)
 
     return {
       data: JSON.parse(JSON.stringify(events)),
       totalPages: Math.ceil(eventsCount / limit),
     }
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// GET EVENTS BY ORGANIZER
+export async function getEventsByUser({ userId, limit = 6, page }: GetEventsByUserParams) {
+  try {
+    await connectToDatabase()
+
+    const conditions = { organizer: userId }
+    const skipAmount = (page - 1) * limit
+
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: 'desc' })
+      .skip(skipAmount)
+      .limit(limit)
+
+    const events = await populateEvent(eventsQuery)
+    const eventsCount = await Event.countDocuments(conditions)
+
+    return { data: JSON.parse(JSON.stringify(events)), totalPages: Math.ceil(eventsCount / limit) }
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// GET RELATED EVENTS: EVENTS WITH SAME CATEGORY
+export async function getRelatedEventsByCategory({
+  categoryId,
+  eventId,
+  limit = 3,
+  page = 1,
+}: GetRelatedEventsByCategoryParams) {
+  try {
+    await connectToDatabase()
+
+    const skipAmount = (Number(page) - 1) * limit
+    const conditions = { $and: [{ category: categoryId }, { _id: { $ne: eventId } }] }
+
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: 'desc' })
+      .skip(skipAmount)
+      .limit(limit)
+
+    const events = await populateEvent(eventsQuery)
+    const eventsCount = await Event.countDocuments(conditions)
+
+    return { data: JSON.parse(JSON.stringify(events)), totalPages: Math.ceil(eventsCount / limit) }
   } catch (error) {
     handleError(error)
   }
